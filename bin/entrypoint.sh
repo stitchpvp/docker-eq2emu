25e3323068cbf448880978c0f005bf370f89baae
Jonmichael Chambers <jon@tyrbo.net>
2018-02-17 17:53:09 -0700

    gzip data

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 rm -rf /release/*
 
-lndir :wq
-
 ln -s /world/source/WorldServer/eq2world /release/eq2world
 ln -s /app/logs /release/logs
 cp -rs /app/structs/*.xml /release
 cp -rs /app/config/* /release
 
 # give db time to start
-sleep 5
+if ! mysql -uroot -peq2world -hdb -e 'show databases'; then
+  echo "Waiting for database..."
+  sleep 10
+fi
 
 if ! mysql -uroot -peq2world -hdb -e 'use eq2world'; then
+  echo "Seeding database, this might take a minute..."
   mysql -uroot -peq2world -hdb -e "create database eq2world"; 
-  mysql -uroot -peq2world -hdb eq2world < /app/data/dump.sql
+  gunzip < /app/data/dump.sql.gz | mysql -uroot -peq2world -hdb eq2world
 fi
 
 exec "$@"
